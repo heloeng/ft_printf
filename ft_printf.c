96174303094367a3fff8e68c752a8fbfe3589dfe
Heloiza De Souza  Mendes <helde-so@c2r1p3.42sp.org.br>
2024-11-23 16:54:58 -0300

    printchar

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: helde-so <helde-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 14:48:45 by helde-so          #+#    #+#             */
-/*   Updated: 2024/11/22 17:34:28 by helde-so         ###   ########.fr       */
+/*   Updated: 2024/11/23 15:30:13 by helde-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,25 @@ int ft_printchar(char c){
 	return write(1, &c, 1);
 }
 
+int ft_print_string(char *str)
+{
+	int    count;
+	int i;
+	if(str == NULL)
+	{
+		return write(1, "(null)", 6);
+	}
+
+count = 0;
+i = 0;
+	while(str[i] != '\0')
+	{
+		count =  count + ft_printchar(str[i]);
+		i++;
+	}
+	return (count);
+}
+
 
 int ft_formats(char c, va_list args)
 {
@@ -28,8 +47,9 @@ int ft_formats(char c, va_list args)
 	count =0;
 	if(c == 'c')
 	count += ft_printchar(va_arg(args, int));
+	else if(c == 's')
+	count += ft_print_string(va_arg(args, char *));
 	return count;
-	
 }
 
 
@@ -67,9 +87,13 @@ int ft_printf(const char *str, ...)
 
 int main(void)
 {
-	char c;
-	c = 'x';
-
-	printf("\n%d", ft_printf("%c", c));
+	//char c;
+	//c = 'x';
+	//printf("\n%d", ft_printf("%c", c));
+	
+	char *str;
+	str = "heloiza";
+	//printf("\n%d", ft_print_string(str));
+	ft_printf("%s\n", str);
 	return(0);
 }
